5704ff65485968e24509238b40c1c8a8544e3f83
vishal sawai <vishalsawai1725@gmail.com>
2024-03-25 00:29:17 +0530

    Heading framer motion added done complete all work

--- src/Components/Heading.jsx
+++ src/Components/Heading.jsx
@@ -1,10 +1,17 @@
+import { motion } from 'framer-motion';
+
 const Heading = ({ title }) => {
     return (
-        <>
+        <motion.div
+            initial={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+        >
             <div>
                 <h1 className="text-2xl text-center font-bold text-red-600 my-5 uppercase">{title}</h1>
             </div>
-        </>
+        </motion.div>
     )
 }
 export default Heading;
