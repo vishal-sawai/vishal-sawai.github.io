795dcdc2170d9130ffa8bb7028ea588c7e3e3b54
vishal sawai <vishalsawai1725@gmail.com>
2024-04-01 20:44:38 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vishal-sawai.github.io",
+  base: "vishalsawai.me",
   plugins: [react()],
 })
