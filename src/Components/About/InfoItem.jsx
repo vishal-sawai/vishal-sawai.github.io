62c15ca7ce42e106d4a3f23f5c56704d1fa025f0
Vishal Sawai <vishalsawai17252@gmail.com>
2024-04-01 22:47:42 +0530

    Delete src directory

--- src/Components/About/InfoItem.jsx
+++
@@ -1,8 +0,0 @@
-const InfoItem = ({ title, content }) => (
-    <div className="mx-2 my-2">
-        <h1 className="text-red-400 text-xl">{title}</h1>
-        <p className="text-lg text-gray-400">{content}</p>
-    </div>
-);
-
-export default InfoItem;
--- src/Components/ProfileImage.jsx
+++
@@ -1,7 +0,0 @@
-import { getImageUrl } from "../Utils/image-util";
-const ProfileImage = ({ url, className }) => (
-    <>
-        <img src={getImageUrl(url)} alt='profile' className={className} />
-    </>
-);
-export default ProfileImage;
